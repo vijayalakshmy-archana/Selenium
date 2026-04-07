87c2c21f31d1625a52a40fea7b0a30c10ada7d48
vijayalakshmy-archana <vijayalakshmy0595@gmail.com>
2026-04-07 12:24:22 +0530

    Add files via upload

---
+++ helper_example.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+def method_name(driver):
+    user_locator = "login-user"
+    password_locator = "login-pass"
+    login_locator = "login-button"
+    expected_title = "Form Fields Automation Testing"
+
+    user_element = driver.find_element(By.ID, user_locator)
+    password_element = driver.find_element(By.ID, password_locator)
+    login_element = driver.find_element(By.ID, login_locator)
+    return expected_title, login_element, password_element, user_element
