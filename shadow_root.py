7fbea72bc9b473c3bb7b13ad5c59d986975990fd
vijayalakshmy-archana <vijayalakshmy0595@gmail.com>
2026-04-07 12:27:42 +0530

    Add files via upload

---
+++ shadow_root.py
@@ -0,0 +1,6 @@
+def click_date(self):
+    #     shadow_host = self.driver.find_element(By.XPATH, "//wdpr-lodging-quickquote[@id='lodging-qq-container']") - parent
+    #     shadow_root = shadow_host.shadow_root
+    #     shadow_content = shadow_root.find_element(By.ID, "findPricesButton") - child
+
+    #for child x path will not work only id, name, tag name or css selector or basic selector
